92390b3aa03e2eb70dfde8f80f812abfd611ad4f
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-12-17 22:12:15 -0100

    added unit test for createBook

--- src/test/java/com/ratz/libraryapi/resource/BookControlTest.java
+++ src/test/java/com/ratz/libraryapi/resource/BookControlTest.java
@@ -1,15 +1,22 @@
 package com.ratz.libraryapi.resource;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
@@ -17,18 +24,32 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 public class BookControlTest {
 
+  static String BOOK_API = "/api/books";
+
   @Autowired
   MockMvc mockMvc;
 
   @Test
   @DisplayName("Should create one book with success")
-  public void createBookTest(){
+  public void createBookTest() throws Exception {
 
+    String json = new ObjectMapper().writeValueAsString(null);
+
+    MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(BOOK_API)
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON)
+        .content(json);
+
+    mockMvc.perform(request)
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("id").isNotEmpty())
+        .andExpect(jsonPath("title").isNotEmpty())
+        .andExpect(jsonPath("author").isNotEmpty());
   }
 
   @Test
   @DisplayName("Should give an error when we dont have all data to create one book")
-  public void createInvalidBookTest(){
+  public void createInvalidBookTest() {
 
   }
 }
