0d1ef95c4880c2a4e8bed645c93638f5ba6bb95f
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-12-29 17:34:20 -0100

    created fiter method on book controller

--- src/main/java/com/ratz/libraryapi/contoller/BookController.java
+++ src/main/java/com/ratz/libraryapi/contoller/BookController.java
@@ -9,6 +9,9 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -18,6 +21,8 @@ import io.swagger.annotations.Api;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -27,64 +32,74 @@ import javax.validation.Valid;
 @Api("Book API")
 public class BookController {
 
-  private final BookService bookService;
-  private final ModelMapper modelMapper;
+    private final BookService bookService;
+    private final ModelMapper modelMapper;
 
 
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  public BookDTO createBook(@RequestBody @Valid BookDTO bookDTO) {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookDTO createBook(@RequestBody @Valid BookDTO bookDTO) {
 
-    Book book = modelMapper.map(bookDTO, Book.class);
-    book = bookService.save(book);
+        Book book = modelMapper.map(bookDTO, Book.class);
+        book = bookService.save(book);
 
-    return modelMapper.map(book, BookDTO.class);
-  }
+        return modelMapper.map(book, BookDTO.class);
+    }
 
-  @ExceptionHandler(MethodArgumentNotValidException.class)
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
-  public ApiErrors handleValidationException(MethodArgumentNotValidException ex) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleValidationException(MethodArgumentNotValidException ex) {
 
-    BindingResult bindingResult = ex.getBindingResult();
-    return new ApiErrors(bindingResult);
-  }
+        BindingResult bindingResult = ex.getBindingResult();
+        return new ApiErrors(bindingResult);
+    }
 
-  @ExceptionHandler(BusinessException.class)
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
-  public ApiErrors handleBusinessException(BusinessException ex){
-    return  new ApiErrors(ex);
-  }
+    @ExceptionHandler(BusinessException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleBusinessException(BusinessException ex) {
+        return new ApiErrors(ex);
+    }
 
-  @GetMapping("/{id}")
-  public BookDTO getBookById(@PathVariable Long id){
+    @GetMapping("/{id}")
+    public BookDTO getBookById(@PathVariable Long id) {
 
-    return bookService.getById(id).map(book -> modelMapper.map(book,BookDTO.class))
-        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-  }
+        return bookService.getById(id).map(book -> modelMapper.map(book, BookDTO.class))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
 
-  @DeleteMapping("/{id}")
-  @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void deleteBookById(@PathVariable Long id){
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBookById(@PathVariable Long id) {
 
-    Book book = bookService.getById(id)
-        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-    bookService.deleteBook(book);
-  }
+        Book book = bookService.getById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        bookService.deleteBook(book);
+    }
 
     @PutMapping("/{id}")
-    public BookDTO updateBook(@PathVariable Long id, BookDTO bookDTO){
+    public BookDTO updateBook(@PathVariable Long id, BookDTO bookDTO) {
 
-      return bookService.getById(id).map(bookToUpdate -> {
+        return bookService.getById(id).map(bookToUpdate -> {
             bookToUpdate.setAuthor(bookDTO.getAuthor());
             bookToUpdate.setTitle(bookDTO.getTitle());
 
             bookToUpdate = bookService.update(bookToUpdate);
-            return modelMapper.map(bookToUpdate,BookDTO.class);
-          })
-          .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+            return modelMapper.map(bookToUpdate, BookDTO.class);
+        })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
+    }
 
+    @GetMapping("")
+    public Page<BookDTO> filterBooks(BookDTO bookDTO, Pageable pageRequest) {
 
+        Book filter = modelMapper.map(bookDTO, Book.class);
+        Page<Book> result = bookService.find(filter, pageRequest);
 
+        List<BookDTO> list = result.getContent().stream()
+                .map(entity -> modelMapper.map(entity, BookDTO.class))
+                .collect(Collectors.toList());
+
+        return new PageImpl<BookDTO>(list, pageRequest, result.getTotalElements());
     }
 }
